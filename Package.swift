fe6742aa4d9b78e6342f9e1dbe81f9edb75fd9b5
Jihoonahn <jihoonahn.dev@gmail.com>
2023-06-17 22:20:26 +0900

    Update Package Macro

--- Package.swift
+++ Package.swift
@@ -1,21 +1,32 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "Builder",
-    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
+    platforms: [.iOS(.v11), .macOS(.v10_15), .tvOS(.v11), .watchOS(.v4), .macCatalyst(.v13)],
     products: [
         .library(
             name: "Builder",
             targets: ["Builder"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.2.0")
     ],
     targets: [
+        .macro(
+            name: "BuilderMacro",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
         .target(
             name: "Builder",
-            dependencies: []),
+            dependencies: [
+                "BuilderMacro"
+            ]),
         .testTarget(
             name: "BuilderTests",
             dependencies: ["Builder"]),
